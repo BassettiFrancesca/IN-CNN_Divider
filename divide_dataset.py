d89db562f892ec8d8f5e53b6e817f8930368c171
Francesca <bassetti.fra@gmail.com>
2021-02-16 11:53:33 +0100

    Added possibility to change groups

--- divide_dataset.py
+++ divide_dataset.py
@@ -3,14 +3,12 @@ import torchvision.transforms as transforms
 import group_dataset
 
 
-def divide_dataset():
+def divide_dataset(groups):
 
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
 
     train_set = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
     test_set = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
-
-    groups = [[1, 2, 4, 5, 7], [0, 3, 6, 8, 9]]
 
     train_d_set = group_dataset.GroupDataset(train_set, groups)
     test_d_set = group_dataset.GroupDataset(test_set, groups)
--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import divide_dataset
 
 def mnist_cnn(train, test):
 
+    groups = [[0, 4, 5, 7, 9], [1, 2, 3, 6, 8]]
+
     start1 = time.time()
 
-    (train_d_set, test_d_set) = divide_dataset.divide_dataset()
+    (train_d_set, test_d_set) = divide_dataset.divide_dataset(groups)
 
     start2 = time.time()
 
@@ -21,6 +23,8 @@ def mnist_cnn(train, test):
         testing.test(test_d_set)
 
     finish = time.time()
+
+    print(f'Groups: {groups}')
 
     print('Total seconds passed: %.3f' % (finish - start1))
     print('Seconds passed for dividing: %.3f' % (start2 - start1))
